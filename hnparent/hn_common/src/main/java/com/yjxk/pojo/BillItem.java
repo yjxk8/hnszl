687ba9fb19d8c0a5fbf4b977407dd2b0deb228cc
yjxk8 <yjxk8@163.com>
2022-05-30 23:16:07 +0800

    Initial commit

---
+++ hnparent/hn_common/src/main/java/com/yjxk/pojo/BillItem.java
@@ -0,0 +1,657 @@
+package com.yjxk.pojo;
+
+
+public class BillItem {
+
+    private String id;
+    private String rekId;
+    private String itemId;
+    private String paiVisitId;
+    private String patientId;
+    private String visitId;
+    private String itemClass;
+    private String itemCode;
+    private String standardItemCode;
+    private String itemName;
+    private String itemSpec;
+    private String amount;
+    private String unitName;
+    private String factor;
+    private String retailFactor;
+    private String quantity;
+    private String unitQuantity;
+    private String execQuantity;
+    private String salesPrice;
+    private String costs;
+    private String charges;
+    private String classOnRcpt;
+    private String refundFlag;
+    private String relRefundId;
+    private String applyClass;
+    private String applyId;
+    private String orderId;
+    private String diagEmpId;
+    private String diagEmpName;
+    private String diagBy;
+    private String serviceLogOrg;
+    private String chargedBy;
+    private String orderedBy;
+    private String performedBy;
+    private String insuranceType;
+    private java.sql.Date chargeDate;
+    private java.sql.Date enterDate;
+    private String invoicePrintStatus;
+    private String branchNo;
+    private String performedDevice;
+    private String subjCode;
+    private String operator;
+    private String executor;
+    private String remark;
+    private String clinicId;
+    private String currentNursingUnit;
+    private String refundApplyFlag;
+    private String settleStatus;
+    private String externalId;
+    private String pretransactId;
+    private String inOutFlag;
+    private String itemUniqueCode;
+    private String insurLevelCode;
+    private String insurLevelName;
+    private String permed;
+    private String freqname;
+    private String duration;
+    private String treatmentGroup;
+    private String medunitname;
+    private String frequnitname;
+    private String durationunitname;
+    private String selfChargeFlag;
+    private String inoutNumber;
+    private String goodsDeptCode;
+    private String hmFlag;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+
+    public String getRekId() {
+        return rekId;
+    }
+
+    public void setRekId(String rekId) {
+        this.rekId = rekId;
+    }
+
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+
+    public String getPaiVisitId() {
+        return paiVisitId;
+    }
+
+    public void setPaiVisitId(String paiVisitId) {
+        this.paiVisitId = paiVisitId;
+    }
+
+
+    public String getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(String patientId) {
+        this.patientId = patientId;
+    }
+
+
+    public String getVisitId() {
+        return visitId;
+    }
+
+    public void setVisitId(String visitId) {
+        this.visitId = visitId;
+    }
+
+
+    public String getItemClass() {
+        return itemClass;
+    }
+
+    public void setItemClass(String itemClass) {
+        this.itemClass = itemClass;
+    }
+
+
+    public String getItemCode() {
+        return itemCode;
+    }
+
+    public void setItemCode(String itemCode) {
+        this.itemCode = itemCode;
+    }
+
+
+    public String getStandardItemCode() {
+        return standardItemCode;
+    }
+
+    public void setStandardItemCode(String standardItemCode) {
+        this.standardItemCode = standardItemCode;
+    }
+
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+
+    public String getItemSpec() {
+        return itemSpec;
+    }
+
+    public void setItemSpec(String itemSpec) {
+        this.itemSpec = itemSpec;
+    }
+
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+
+    public String getUnitName() {
+        return unitName;
+    }
+
+    public void setUnitName(String unitName) {
+        this.unitName = unitName;
+    }
+
+
+    public String getFactor() {
+        return factor;
+    }
+
+    public void setFactor(String factor) {
+        this.factor = factor;
+    }
+
+
+    public String getRetailFactor() {
+        return retailFactor;
+    }
+
+    public void setRetailFactor(String retailFactor) {
+        this.retailFactor = retailFactor;
+    }
+
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+
+
+    public String getUnitQuantity() {
+        return unitQuantity;
+    }
+
+    public void setUnitQuantity(String unitQuantity) {
+        this.unitQuantity = unitQuantity;
+    }
+
+
+    public String getExecQuantity() {
+        return execQuantity;
+    }
+
+    public void setExecQuantity(String execQuantity) {
+        this.execQuantity = execQuantity;
+    }
+
+
+    public String getSalesPrice() {
+        return salesPrice;
+    }
+
+    public void setSalesPrice(String salesPrice) {
+        this.salesPrice = salesPrice;
+    }
+
+
+    public String getCosts() {
+        return costs;
+    }
+
+    public void setCosts(String costs) {
+        this.costs = costs;
+    }
+
+
+    public String getCharges() {
+        return charges;
+    }
+
+    public void setCharges(String charges) {
+        this.charges = charges;
+    }
+
+
+    public String getClassOnRcpt() {
+        return classOnRcpt;
+    }
+
+    public void setClassOnRcpt(String classOnRcpt) {
+        this.classOnRcpt = classOnRcpt;
+    }
+
+
+    public String getRefundFlag() {
+        return refundFlag;
+    }
+
+    public void setRefundFlag(String refundFlag) {
+        this.refundFlag = refundFlag;
+    }
+
+
+    public String getRelRefundId() {
+        return relRefundId;
+    }
+
+    public void setRelRefundId(String relRefundId) {
+        this.relRefundId = relRefundId;
+    }
+
+
+    public String getApplyClass() {
+        return applyClass;
+    }
+
+    public void setApplyClass(String applyClass) {
+        this.applyClass = applyClass;
+    }
+
+
+    public String getApplyId() {
+        return applyId;
+    }
+
+    public void setApplyId(String applyId) {
+        this.applyId = applyId;
+    }
+
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(String orderId) {
+        this.orderId = orderId;
+    }
+
+
+    public String getDiagEmpId() {
+        return diagEmpId;
+    }
+
+    public void setDiagEmpId(String diagEmpId) {
+        this.diagEmpId = diagEmpId;
+    }
+
+
+    public String getDiagEmpName() {
+        return diagEmpName;
+    }
+
+    public void setDiagEmpName(String diagEmpName) {
+        this.diagEmpName = diagEmpName;
+    }
+
+
+    public String getDiagBy() {
+        return diagBy;
+    }
+
+    public void setDiagBy(String diagBy) {
+        this.diagBy = diagBy;
+    }
+
+
+    public String getServiceLogOrg() {
+        return serviceLogOrg;
+    }
+
+    public void setServiceLogOrg(String serviceLogOrg) {
+        this.serviceLogOrg = serviceLogOrg;
+    }
+
+
+    public String getChargedBy() {
+        return chargedBy;
+    }
+
+    public void setChargedBy(String chargedBy) {
+        this.chargedBy = chargedBy;
+    }
+
+
+    public String getOrderedBy() {
+        return orderedBy;
+    }
+
+    public void setOrderedBy(String orderedBy) {
+        this.orderedBy = orderedBy;
+    }
+
+
+    public String getPerformedBy() {
+        return performedBy;
+    }
+
+    public void setPerformedBy(String performedBy) {
+        this.performedBy = performedBy;
+    }
+
+
+    public String getInsuranceType() {
+        return insuranceType;
+    }
+
+    public void setInsuranceType(String insuranceType) {
+        this.insuranceType = insuranceType;
+    }
+
+
+    public java.sql.Date getChargeDate() {
+        return chargeDate;
+    }
+
+    public void setChargeDate(java.sql.Date chargeDate) {
+        this.chargeDate = chargeDate;
+    }
+
+
+    public java.sql.Date getEnterDate() {
+        return enterDate;
+    }
+
+    public void setEnterDate(java.sql.Date enterDate) {
+        this.enterDate = enterDate;
+    }
+
+
+    public String getInvoicePrintStatus() {
+        return invoicePrintStatus;
+    }
+
+    public void setInvoicePrintStatus(String invoicePrintStatus) {
+        this.invoicePrintStatus = invoicePrintStatus;
+    }
+
+
+    public String getBranchNo() {
+        return branchNo;
+    }
+
+    public void setBranchNo(String branchNo) {
+        this.branchNo = branchNo;
+    }
+
+
+    public String getPerformedDevice() {
+        return performedDevice;
+    }
+
+    public void setPerformedDevice(String performedDevice) {
+        this.performedDevice = performedDevice;
+    }
+
+
+    public String getSubjCode() {
+        return subjCode;
+    }
+
+    public void setSubjCode(String subjCode) {
+        this.subjCode = subjCode;
+    }
+
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+
+
+    public String getExecutor() {
+        return executor;
+    }
+
+    public void setExecutor(String executor) {
+        this.executor = executor;
+    }
+
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
+    }
+
+
+    public String getClinicId() {
+        return clinicId;
+    }
+
+    public void setClinicId(String clinicId) {
+        this.clinicId = clinicId;
+    }
+
+
+    public String getCurrentNursingUnit() {
+        return currentNursingUnit;
+    }
+
+    public void setCurrentNursingUnit(String currentNursingUnit) {
+        this.currentNursingUnit = currentNursingUnit;
+    }
+
+
+    public String getRefundApplyFlag() {
+        return refundApplyFlag;
+    }
+
+    public void setRefundApplyFlag(String refundApplyFlag) {
+        this.refundApplyFlag = refundApplyFlag;
+    }
+
+
+    public String getSettleStatus() {
+        return settleStatus;
+    }
+
+    public void setSettleStatus(String settleStatus) {
+        this.settleStatus = settleStatus;
+    }
+
+
+    public String getExternalId() {
+        return externalId;
+    }
+
+    public void setExternalId(String externalId) {
+        this.externalId = externalId;
+    }
+
+
+    public String getPretransactId() {
+        return pretransactId;
+    }
+
+    public void setPretransactId(String pretransactId) {
+        this.pretransactId = pretransactId;
+    }
+
+
+    public String getInOutFlag() {
+        return inOutFlag;
+    }
+
+    public void setInOutFlag(String inOutFlag) {
+        this.inOutFlag = inOutFlag;
+    }
+
+
+    public String getItemUniqueCode() {
+        return itemUniqueCode;
+    }
+
+    public void setItemUniqueCode(String itemUniqueCode) {
+        this.itemUniqueCode = itemUniqueCode;
+    }
+
+
+    public String getInsurLevelCode() {
+        return insurLevelCode;
+    }
+
+    public void setInsurLevelCode(String insurLevelCode) {
+        this.insurLevelCode = insurLevelCode;
+    }
+
+
+    public String getInsurLevelName() {
+        return insurLevelName;
+    }
+
+    public void setInsurLevelName(String insurLevelName) {
+        this.insurLevelName = insurLevelName;
+    }
+
+
+    public String getPermed() {
+        return permed;
+    }
+
+    public void setPermed(String permed) {
+        this.permed = permed;
+    }
+
+
+    public String getFreqname() {
+        return freqname;
+    }
+
+    public void setFreqname(String freqname) {
+        this.freqname = freqname;
+    }
+
+
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+
+    public String getTreatmentGroup() {
+        return treatmentGroup;
+    }
+
+    public void setTreatmentGroup(String treatmentGroup) {
+        this.treatmentGroup = treatmentGroup;
+    }
+
+
+    public String getMedunitname() {
+        return medunitname;
+    }
+
+    public void setMedunitname(String medunitname) {
+        this.medunitname = medunitname;
+    }
+
+
+    public String getFrequnitname() {
+        return frequnitname;
+    }
+
+    public void setFrequnitname(String frequnitname) {
+        this.frequnitname = frequnitname;
+    }
+
+
+    public String getDurationunitname() {
+        return durationunitname;
+    }
+
+    public void setDurationunitname(String durationunitname) {
+        this.durationunitname = durationunitname;
+    }
+
+
+    public String getSelfChargeFlag() {
+        return selfChargeFlag;
+    }
+
+    public void setSelfChargeFlag(String selfChargeFlag) {
+        this.selfChargeFlag = selfChargeFlag;
+    }
+
+
+    public String getInoutNumber() {
+        return inoutNumber;
+    }
+
+    public void setInoutNumber(String inoutNumber) {
+        this.inoutNumber = inoutNumber;
+    }
+
+
+    public String getGoodsDeptCode() {
+        return goodsDeptCode;
+    }
+
+    public void setGoodsDeptCode(String goodsDeptCode) {
+        this.goodsDeptCode = goodsDeptCode;
+    }
+
+
+    public String getHmFlag() {
+        return hmFlag;
+    }
+
+    public void setHmFlag(String hmFlag) {
+        this.hmFlag = hmFlag;
+    }
+
+}
---
+++ hnparent/hn_common/src/main/java/com/yjxk/pojo/Rek.java
@@ -0,0 +1,367 @@
+package com.yjxk.pojo;
+
+
+public class Rek {
+
+  private String id;
+  private String rekNo;
+  private String rekType;
+  private String cardNo;
+  private String paiVisitId;
+  private String patientId;
+  private String visitId;
+  private String chargeType;
+  private java.sql.Date rekDate;
+  private String costs;
+  private String charges;
+  private String operator;
+  private String printTimes;
+  private String invoiceFlag;
+  private String rekNeg;
+  private String footId;
+  private java.sql.Date cancDate;
+  private String cancOperator;
+  private String cancReason;
+  private String area;
+  private String patientName;
+  private String terminalType;
+  private String settleType;
+  private String debtFlag;
+  private java.sql.Date startDate;
+  private java.sql.Date endDate;
+  private String settleWay;
+  private String note;
+  private String inOutFlag;
+  private String contractUnit;
+  private String contractNo;
+  private String transactionSn;
+  private String actingCapture;
+  private String refundReason;
+  private String prepayment;
+  private String fillamount;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getRekNo() {
+    return rekNo;
+  }
+
+  public void setRekNo(String rekNo) {
+    this.rekNo = rekNo;
+  }
+
+
+  public String getRekType() {
+    return rekType;
+  }
+
+  public void setRekType(String rekType) {
+    this.rekType = rekType;
+  }
+
+
+  public String getCardNo() {
+    return cardNo;
+  }
+
+  public void setCardNo(String cardNo) {
+    this.cardNo = cardNo;
+  }
+
+
+  public String getPaiVisitId() {
+    return paiVisitId;
+  }
+
+  public void setPaiVisitId(String paiVisitId) {
+    this.paiVisitId = paiVisitId;
+  }
+
+
+  public String getPatientId() {
+    return patientId;
+  }
+
+  public void setPatientId(String patientId) {
+    this.patientId = patientId;
+  }
+
+
+  public String getVisitId() {
+    return visitId;
+  }
+
+  public void setVisitId(String visitId) {
+    this.visitId = visitId;
+  }
+
+
+  public String getChargeType() {
+    return chargeType;
+  }
+
+  public void setChargeType(String chargeType) {
+    this.chargeType = chargeType;
+  }
+
+
+  public java.sql.Date getRekDate() {
+    return rekDate;
+  }
+
+  public void setRekDate(java.sql.Date rekDate) {
+    this.rekDate = rekDate;
+  }
+
+
+  public String getCosts() {
+    return costs;
+  }
+
+  public void setCosts(String costs) {
+    this.costs = costs;
+  }
+
+
+  public String getCharges() {
+    return charges;
+  }
+
+  public void setCharges(String charges) {
+    this.charges = charges;
+  }
+
+
+  public String getOperator() {
+    return operator;
+  }
+
+  public void setOperator(String operator) {
+    this.operator = operator;
+  }
+
+
+  public String getPrintTimes() {
+    return printTimes;
+  }
+
+  public void setPrintTimes(String printTimes) {
+    this.printTimes = printTimes;
+  }
+
+
+  public String getInvoiceFlag() {
+    return invoiceFlag;
+  }
+
+  public void setInvoiceFlag(String invoiceFlag) {
+    this.invoiceFlag = invoiceFlag;
+  }
+
+
+  public String getRekNeg() {
+    return rekNeg;
+  }
+
+  public void setRekNeg(String rekNeg) {
+    this.rekNeg = rekNeg;
+  }
+
+
+  public String getFootId() {
+    return footId;
+  }
+
+  public void setFootId(String footId) {
+    this.footId = footId;
+  }
+
+
+  public java.sql.Date getCancDate() {
+    return cancDate;
+  }
+
+  public void setCancDate(java.sql.Date cancDate) {
+    this.cancDate = cancDate;
+  }
+
+
+  public String getCancOperator() {
+    return cancOperator;
+  }
+
+  public void setCancOperator(String cancOperator) {
+    this.cancOperator = cancOperator;
+  }
+
+
+  public String getCancReason() {
+    return cancReason;
+  }
+
+  public void setCancReason(String cancReason) {
+    this.cancReason = cancReason;
+  }
+
+
+  public String getArea() {
+    return area;
+  }
+
+  public void setArea(String area) {
+    this.area = area;
+  }
+
+
+  public String getPatientName() {
+    return patientName;
+  }
+
+  public void setPatientName(String patientName) {
+    this.patientName = patientName;
+  }
+
+
+  public String getTerminalType() {
+    return terminalType;
+  }
+
+  public void setTerminalType(String terminalType) {
+    this.terminalType = terminalType;
+  }
+
+
+  public String getSettleType() {
+    return settleType;
+  }
+
+  public void setSettleType(String settleType) {
+    this.settleType = settleType;
+  }
+
+
+  public String getDebtFlag() {
+    return debtFlag;
+  }
+
+  public void setDebtFlag(String debtFlag) {
+    this.debtFlag = debtFlag;
+  }
+
+
+  public java.sql.Date getStartDate() {
+    return startDate;
+  }
+
+  public void setStartDate(java.sql.Date startDate) {
+    this.startDate = startDate;
+  }
+
+
+  public java.sql.Date getEndDate() {
+    return endDate;
+  }
+
+  public void setEndDate(java.sql.Date endDate) {
+    this.endDate = endDate;
+  }
+
+
+  public String getSettleWay() {
+    return settleWay;
+  }
+
+  public void setSettleWay(String settleWay) {
+    this.settleWay = settleWay;
+  }
+
+
+  public String getNote() {
+    return note;
+  }
+
+  public void setNote(String note) {
+    this.note = note;
+  }
+
+
+  public String getInOutFlag() {
+    return inOutFlag;
+  }
+
+  public void setInOutFlag(String inOutFlag) {
+    this.inOutFlag = inOutFlag;
+  }
+
+
+  public String getContractUnit() {
+    return contractUnit;
+  }
+
+  public void setContractUnit(String contractUnit) {
+    this.contractUnit = contractUnit;
+  }
+
+
+  public String getContractNo() {
+    return contractNo;
+  }
+
+  public void setContractNo(String contractNo) {
+    this.contractNo = contractNo;
+  }
+
+
+  public String getTransactionSn() {
+    return transactionSn;
+  }
+
+  public void setTransactionSn(String transactionSn) {
+    this.transactionSn = transactionSn;
+  }
+
+
+  public String getActingCapture() {
+    return actingCapture;
+  }
+
+  public void setActingCapture(String actingCapture) {
+    this.actingCapture = actingCapture;
+  }
+
+
+  public String getRefundReason() {
+    return refundReason;
+  }
+
+  public void setRefundReason(String refundReason) {
+    this.refundReason = refundReason;
+  }
+
+
+  public String getPrepayment() {
+    return prepayment;
+  }
+
+  public void setPrepayment(String prepayment) {
+    this.prepayment = prepayment;
+  }
+
+
+  public String getFillamount() {
+    return fillamount;
+  }
+
+  public void setFillamount(String fillamount) {
+    this.fillamount = fillamount;
+  }
+
+}
